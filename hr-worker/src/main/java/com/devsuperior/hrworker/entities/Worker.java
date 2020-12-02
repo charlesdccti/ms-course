655c801e74e8700e5b5a4214ec8b54a5c3823e70
Charles  Ferreira <charlesdccti@gmail.com~>
2020-12-02 12:05:17 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -0,0 +1,29 @@
+package com.devsuperior.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+	
+	
+	
+}
