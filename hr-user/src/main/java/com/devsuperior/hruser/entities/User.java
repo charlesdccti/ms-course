bd047d1a464968f9372af33e975ea12a62c0d21d
Charles  Ferreira <charlesdccti@gmail.com~>
2020-12-05 11:55:42 -0300

    UserRespozitory, UserResource and Zuul config

--- hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
@@ -15,6 +15,15 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -36,8 +45,6 @@ public class User implements Serializable {
 	)
 	private Set<Role> roles = new HashSet<>();
 	
-	public User() {
-	}
 
 	public User(Long id, String name, String email, String password) {
 		super();
@@ -47,64 +54,4 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 }
