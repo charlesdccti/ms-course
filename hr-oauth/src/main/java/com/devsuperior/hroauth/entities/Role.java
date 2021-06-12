71d45422094edb30333d24cfbbfcbc0a35c98ab5
Charles  Ferreira <charlesdccti@gmail.com>
2021-06-12 13:26:53 -0300

    Add project oauth correct

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/Role.java
@@ -0,0 +1,20 @@
+package com.devsuperior.hroauth.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String roleName;
+
+}
